a0d51627e0c1835ecccb2ed85fdf8c2fdd29dfb0
RodZill4 <rodzilla@free.fr>
2018-09-14 21:17:38 +0200

    Simplified multimesh generation

--- addons/procedural_maze/maze_multimesh.gd
+++ addons/procedural_maze/maze_multimesh.gd
@@ -3,78 +3,57 @@ extends "res://addons/procedural_maze/maze.gd"
 
 export(Array) var wall_models = []
 
+class MultiMeshBuilder:
+	var mesh
+	var instances = []
+	
+	func _init(m):
+		mesh = m
+	
+	func add(position, rotation):
+		instances.append({ position=position, rotation=rotation })
+	
+	func finalize(parent):
+		var multi_mesh = MultiMesh.new()
+		multi_mesh.mesh = mesh
+		multi_mesh.color_format = MultiMesh.COLOR_NONE
+		multi_mesh.transform_format = MultiMesh.TRANSFORM_3D
+		multi_mesh.instance_count = instances.size()
+		print(str(multi_mesh.instance_count)+" instances")
+		for i in range(instances.size()):
+			var instance = instances[i]
+			var basis = Basis().rotated(Vector3(0, 1, 0), instance.rotation)
+			multi_mesh.set_instance_transform(i, Transform(basis, instance.position))
+		var multi_mesh_instance = MultiMeshInstance.new()
+		multi_mesh_instance.multimesh = multi_mesh
+		parent.add_child(multi_mesh_instance)
+
 func generate_walls_mesh(generator, builder):
 	# Create multi_mesh
-	var multi_mesh = []
-	var count = PoolIntArray()
-	var index = PoolIntArray()
-	for i in range(4):
-		multi_mesh.append(MultiMesh.new())
-		count.append(0)
-		index.append(0)
-	seed(0)
-	var all_pillars = []
-	for i in range(0, generator.walls_x.size(), 3):
-		var y = generator.walls_x[i]
-		var x1 = generator.walls_x[i+1]
-		var x2 = generator.walls_x[i+2]
-		for j in range(x1, x2+1):
-			var model = randi()%3
-			count[model] += 1
-		all_pillars.append(Vector2(x1, y))
-		all_pillars.append(Vector2(x2+1, y))
-	for i in range(0, generator.walls_y.size(), 3):
-		var x = generator.walls_y[i]
-		var y1 = generator.walls_y[i+1]
-		var y2 = generator.walls_y[i+2]
-		for j in range(y1, y2+1):
-			var model = randi()%3
-			count[model] += 1
-		all_pillars.append(Vector2(x, y1))
-		all_pillars.append(Vector2(x, y2+1))
-	for i in range(3):
-		multi_mesh[i].color_format = MultiMesh.COLOR_NONE
-		multi_mesh[i].transform_format = MultiMesh.TRANSFORM_3D
-		multi_mesh[i].instance_count = count[i]
-		multi_mesh[i].mesh = load("res://test_procedural_maze/models/wall"+str(i+1)+".tres")
-	print(all_pillars.size())
-	all_pillars.sort()
-	var pillars = PoolVector2Array()
-	var last = null
-	while !all_pillars.empty():
-		var element = all_pillars.pop_back()
-		if element != last:
-			pillars.append(element)
-		last = element
-	count[3] = pillars.size()
-	print(count[3])
-	multi_mesh[3].color_format = MultiMesh.COLOR_NONE
-	multi_mesh[3].transform_format = MultiMesh.TRANSFORM_3D
-	multi_mesh[3].instance_count = count[3]
-	multi_mesh[3].mesh = load("res://test_procedural_maze/models/pillar.tres")
-	seed(0)
-	var basis = Basis()
+	var outer_walls = []
+	var inner_walls = []
+	var pillars = MultiMeshBuilder.new(load("res://test_procedural_maze/models/pillar.tres"))
+	for m in wall_models:
+		print(m)
+		inner_walls.append(MultiMeshBuilder.new(m))
 	for i in range(0, generator.walls_x.size(), 3):
 		var y = generator.walls_x[i]
 		var x1 = generator.walls_x[i+1]
 		var x2 = generator.walls_x[i+2]
 		for x in range(x1, x2+1):
-			var model = randi()%3
-			multi_mesh[model].set_instance_transform(index[model], Transform(basis, corridor_width*(Vector3(x, 0, y-0.5))))
-			index[model] += 1
-	basis = basis.rotated(Vector3(0, 1, 0), 0.5*PI)
+			var model = randi()%inner_walls.size()
+			inner_walls[model].add(corridor_width*(Vector3(x, 0, y-0.5)), 0)
+		pillars.add(corridor_width*(Vector3(x1-0.5, 0, y-0.5)), 0)
+		pillars.add(corridor_width*(Vector3(x2+0.5, 0, y-0.5)), 0)
 	for i in range(0, generator.walls_y.size(), 3):
 		var x = generator.walls_y[i]
 		var y1 = generator.walls_y[i+1]
 		var y2 = generator.walls_y[i+2]
 		for y in range(y1, y2+1):
-			var model = randi()%3
-			multi_mesh[model].set_instance_transform(index[model], Transform(basis, corridor_width*(Vector3(x-0.5, 0, y))))
-			index[model] += 1
-	for i in pillars:
-		multi_mesh[3].set_instance_transform(index[3], Transform(basis, corridor_width*(Vector3(i.x-0.5, 0, i.y-0.5))))
-		index[3] += 1
-	for i in range(4):
-		var mesh = MultiMeshInstance.new()
-		mesh.multimesh = multi_mesh[i]
-		add_child(mesh)
+			var model = randi()%inner_walls.size()
+			inner_walls[model].add(corridor_width*(Vector3(x-0.5, 0, y)), 0.5*PI)
+		pillars.add(corridor_width*(Vector3(x-0.5, 0, y1-0.5)), 0)
+		pillars.add(corridor_width*(Vector3(x-0.5, 0, y2+0.5)), 0)
+	for i in inner_walls:
+		i.finalize(self)
+	pillars.finalize(self)
