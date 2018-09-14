572a961ec4faff306139323d7da8f602d4ffe493
RodZill4 <rodzilla@free.fr>
2018-09-14 08:33:33 +0200

    Refactored and started adding MultiMesh based walls

--- addons/procedural_maze/maze.gd
+++ addons/procedural_maze/maze.gd
@@ -74,9 +74,165 @@ func generate():
 					generator.open(rect)
 	var time = OS.get_unix_time()
 	generator.generate(random_seed)
+	generator.generate_walls()
 	print("Generated maze in %d seconds" % (OS.get_unix_time() - time))
 	time = OS.get_unix_time()
-	generator.generate_floor(self, floor_material)
-	generator.generate_walls(self, wall_material)
-	generator.generate_ceiling(self, ceiling_material)
-	print("Generated meshes and collision shapes in %d seconds" % (OS.get_unix_time() - time))
+	generate_collision_shapes(generator)
+	print("Generated collision shapes in %d seconds" % (OS.get_unix_time() - time))
+	time = OS.get_unix_time()
+	generate_meshes(generator)
+	print("Generated meshes in %d seconds" % (OS.get_unix_time() - time))
+
+func generate_walls(generator, builder):
+	for i in range(0, generator.walls_x.size(), 3):
+		var y = generator.walls_x[i]
+		var x1 = generator.walls_x[i+1]
+		var x2 = generator.walls_x[i+2]
+		builder.create_wall(corridor_width*(Vector2(x1-0.5, y-0.5))-0.5*wall_width*Vector2(1, 1), corridor_width*(Vector2(x2+0.5, y-0.5))+0.5*wall_width*Vector2(1, 1), height)
+	for i in range(0, generator.walls_y.size(), 3):
+		var x = generator.walls_y[i]
+		var y1 = generator.walls_y[i+1]
+		var y2 = generator.walls_y[i+2]
+		builder.create_wall(corridor_width*(Vector2(x-0.5, y1-0.5))-0.5*wall_width*Vector2(1, 1), corridor_width*(Vector2(x-0.5, y2+0.5))+0.5*wall_width*Vector2(1, 1), height)
+
+class CollisionBuilder:
+	extends Node
+	
+	var collision_object
+	var collision_shape
+	var shapes = 0
+	
+	func _init(o):
+		collision_object = o
+		collision_shape = CollisionShape.new()
+		collision_object.add_child(collision_shape)
+	
+	func add_shape(center, extent, low, height):
+		if shapes >= 50:
+			collision_shape = CollisionShape.new()
+			collision_object.add_child(collision_shape)
+			shapes = 0
+		else:
+			shapes += 1
+		var shape_owner = collision_object.create_shape_owner(collision_shape)
+		var shape = BoxShape.new()
+		shape.extents = Vector3(extent.x, 0.5*height, extent.y)
+		var transform = Transform().translated(Vector3(center.x, low+0.5*height, center.y))
+		collision_object.shape_owner_set_transform(shape_owner, transform)
+		collision_object.shape_owner_add_shape(shape_owner, shape)
+	
+	func create_wall(from, to, height):
+		var center = 0.5*(from+to)
+		var extent = to-center
+		add_shape(center, extent, 0, height)
+
+func generate_collision_shapes(generator):
+	if Engine.editor_hint:
+		return
+	var builder = CollisionBuilder.new(self)
+	# Generate floor
+	builder.add_shape(Vector2(0.5*corridor_width*size_x, 0.5*corridor_width*size_y), Vector2(0.5*corridor_width*(size_x+1), 0.5*corridor_width*(size_y+1)), -1, 1)
+	# Generate walls
+	generate_walls(generator, builder)
+
+class MeshBuilder:
+	extends Node
+	
+	var vertices
+	var uvs
+	var normals
+	var tangents
+	
+	func _init():
+		clear()
+	
+	func clear():
+		vertices = PoolVector3Array()
+		uvs = PoolVector2Array()
+		normals = PoolVector3Array()
+		tangents = PoolRealArray()
+	
+	func add_point(vertex, uv, normal, tangent):
+		vertices.append(vertex)
+		uvs.append(uv)
+		normals.append(normal)
+		tangents.append(tangent[0])
+		tangents.append(tangent[1])
+		tangents.append(tangent[2])
+		tangents.append(tangent[3])
+	
+	func create_vertical_rect(from, to, height):
+		var uv_from = from.y
+		var uv_to = to.y
+		if abs(from.x-to.x) > abs(from.y-to.y):
+			uv_from = from.x
+			uv_to = to.x
+		var normal = Vector3(from.y-to.y, 0, to.x-from.x).normalized()
+		var tangent = [normal.z, 0, -normal.x, 1]
+		add_point(Vector3(from.x, 0, from.y), Vector2(uv_from, 0), normal, tangent)
+		add_point(Vector3(from.x, height, from.y), Vector2(uv_from, height), normal, tangent)
+		add_point(Vector3(to.x, 0, to.y), Vector2(uv_to, 0), normal, tangent)
+		add_point(Vector3(from.x, height, from.y), Vector2(uv_from, height), normal, tangent)
+		add_point(Vector3(to.x, height, to.y), Vector2(uv_to, height), normal, tangent)
+		add_point(Vector3(to.x, 0, to.y), Vector2(uv_to, 0), normal, tangent)
+	
+	func create_wall(from, to, height):
+		create_vertical_rect(Vector2(from.x, from.y), Vector2(from.x, to.y), height)
+		create_vertical_rect(Vector2(from.x, to.y), Vector2(to.x, to.y), height)
+		create_vertical_rect(Vector2(to.x, to.y), Vector2(to.x, from.y), height)
+		create_vertical_rect(Vector2(to.x, from.y), Vector2(from.x, from.y), height)
+
+func generate_floor_mesh(builder):
+	# Generate mesh
+	if floor_material != null:
+		builder.clear()
+		var p1 = -(corridor_width+wall_width)*0.5*Vector2(1, 1)
+		var p2 = corridor_width*Vector2(size_x-1, size_y-1)-p1
+		var normal = Vector3(0, 1, 0)
+		var tangent = [ 1, 0, 0, 1 ]
+		builder.add_point(Vector3(p1.x, 0, p1.y), Vector2(p1.x, p1.y), normal, tangent)
+		builder.add_point(Vector3(p2.x, 0, p1.y), Vector2(p2.x, p1.y), normal, tangent)
+		builder.add_point(Vector3(p1.x, 0, p2.y), Vector2(p1.x, p2.y), normal, tangent)
+		builder.add_point(Vector3(p2.x, 0, p2.y), Vector2(p2.x, p2.y), normal, tangent)
+		var mesh = MeshInstance.new()
+		mesh.name = "floor"
+		mesh.mesh = ArrayMesh.new()
+		mesh.mesh.add_surface_from_arrays(ArrayMesh.PRIMITIVE_TRIANGLE_STRIP, [ builder.vertices, builder.normals, builder.tangents, null, builder.uvs, null, null, null, null ])
+		mesh.mesh.surface_set_material(0, floor_material)
+		add_child(mesh)
+
+func generate_ceiling_mesh(builder):
+	if ceiling_material != null:
+		builder.clear()
+		var p1 = -(corridor_width+wall_width)*0.5*Vector2(1, 1)
+		var p2 = corridor_width*Vector2(size_x-1, size_y-1)-p1
+		var normal = Vector3(0, -1, 0)
+		var tangent = [ 1, 0, 0, 1 ]
+		builder.add_point(Vector3(p1.x, height, p1.y), Vector2(p1.x, p1.y), normal, tangent)
+		builder.add_point(Vector3(p1.x, height, p2.y), Vector2(p1.x, p2.y), normal, tangent)
+		builder.add_point(Vector3(p2.x, height, p1.y), Vector2(p2.x, p1.y), normal, tangent)
+		builder.add_point(Vector3(p2.x, height, p2.y), Vector2(p2.x, p2.y), normal, tangent)
+		var mesh = MeshInstance.new()
+		mesh.name = "floor"
+		mesh.mesh = ArrayMesh.new()
+		mesh.mesh.add_surface_from_arrays(ArrayMesh.PRIMITIVE_TRIANGLE_STRIP, [ builder.vertices, builder.normals, builder.tangents, null, builder.uvs, null, null, null, null ])
+		mesh.mesh.surface_set_material(0, ceiling_material)
+		add_child(mesh)
+
+func generate_walls_mesh(generator, builder):
+	if wall_material != null:
+		builder.clear()
+		generate_walls(generator, builder)
+		var mesh = MeshInstance.new()
+		mesh.name = "walls"
+		mesh.mesh = ArrayMesh.new()
+		mesh.mesh.add_surface_from_arrays(ArrayMesh.PRIMITIVE_TRIANGLES, [ builder.vertices, builder.normals, builder.tangents, null, builder.uvs, null, null, null, null ])
+		mesh.mesh.surface_set_material(0, wall_material)
+		add_child(mesh)
+
+func generate_meshes(generator):
+	var builder = MeshBuilder.new()
+	generate_floor_mesh(builder)
+	generate_ceiling_mesh(builder)
+	generate_walls_mesh(generator, builder)
+
--- addons/procedural_maze/maze_generator.gd
+++ addons/procedural_maze/maze_generator.gd
@@ -7,6 +7,9 @@ var corridor_width
 var wall_width
 var height
 
+var walls_x
+var walls_y
+
 var cells = []
 var loot = []
 
@@ -25,7 +28,7 @@ func initialize(_size_x, _size_y, _corridor_width, _wall_width, _height):
 	height = _height
 	cells = []
 	for x in range(size_x):
-		var line = []
+		var line = PoolIntArray()
 		for y in range(size_y):
 			line.append(0)
 		cells.append(line)
@@ -82,160 +85,44 @@ func generate(random_seed):
 	loot.append( { x=current.x, y=current.y, loot="loot3" } )
 	return loot
 
-class MeshBuilder:
-	extends Node
-	
-	var collision_object
-	var collision_shape
-	var vertices
-	var uvs
-	var normals
-	var tangents
-	
-	func _init(o):
-		collision_object = o
-		collision_shape = CollisionShape.new()
-		collision_object.add_child(collision_shape)
-		vertices = PoolVector3Array()
-		uvs = PoolVector2Array()
-		normals = PoolVector3Array()
-		tangents = PoolRealArray()
-
-	func add_point(vertex, uv, normal, tangent):
-		vertices.append(vertex)
-		uvs.append(uv)
-		normals.append(normal)
-		tangents.append(tangent[0])
-		tangents.append(tangent[1])
-		tangents.append(tangent[2])
-		tangents.append(tangent[3])
-
-	func create_collision_shape_old(center, extent, height):
-		if Engine.editor_hint:
-			return
-		var shape = CollisionShape.new()
-		shape.translation = Vector3(center.x, 0.5*height, center.y)
-		shape.shape = BoxShape.new()
-		shape.shape.extents = Vector3(extent.x, 0.5*height, extent.y)
-		collision_object.add_child(shape)
-
-	func create_collision_shape(center, extent, height):
-		if Engine.editor_hint:
-			return
-		var shape_owner = collision_object.create_shape_owner(collision_shape)
-		var shape = BoxShape.new()
-		shape.extents = Vector3(extent.x, 0.5*height, extent.y)
-		var transform = Transform().translated(Vector3(center.x, 0.5*height, center.y))
-		collision_object.shape_owner_set_transform(shape_owner, transform)
-		collision_object.shape_owner_add_shape(shape_owner, shape)
-
-func generate_floor(maze, material):
-	# Generate collision shape
-	var shape = CollisionShape.new()
-	shape.translation = Vector3(0.5*corridor_width*size_x, -0.5, 0.5*corridor_width*size_y)
-	shape.shape = BoxShape.new()
-	shape.shape.extents = Vector3(0.5*corridor_width*(size_x+1), 0.5, 0.5*corridor_width*(size_y+1))
-	maze.add_child(shape)
-	# Generate mesh
-	if material != null:
-		var context = MeshBuilder.new(maze)
-		print(context.vertices)
-		var p1 = -(corridor_width+wall_width)*0.5*Vector2(1, 1)
-		var p2 = corridor_width*Vector2(size_x-1, size_y-1)-p1
-		var normal = Vector3(0, 1, 0)
-		var tangent = [ 1, 0, 0, 1 ]
-		context.add_point(Vector3(p1.x, 0, p1.y), Vector2(p1.x, p1.y), normal, tangent)
-		context.add_point(Vector3(p2.x, 0, p1.y), Vector2(p2.x, p1.y), normal, tangent)
-		context.add_point(Vector3(p1.x, 0, p2.y), Vector2(p1.x, p2.y), normal, tangent)
-		context.add_point(Vector3(p2.x, 0, p2.y), Vector2(p2.x, p2.y), normal, tangent)
-		var mesh = MeshInstance.new()
-		mesh.name = "floor"
-		mesh.mesh = ArrayMesh.new()
-		mesh.mesh.add_surface_from_arrays(ArrayMesh.PRIMITIVE_TRIANGLE_STRIP, [ context.vertices, context.normals, context.tangents, null, context.uvs, null, null, null, null ])
-		mesh.mesh.surface_set_material(0, material)
-		maze.add_child(mesh)
-
-func generate_walls(maze, material):
-	if material == null:
-		material = SpatialMaterial.new()
-	var context = MeshBuilder.new(maze)
-	generate_walls_along_x(maze, 0, DIRECTION_UP, context)
+func generate_walls():
+	walls_x = PoolIntArray()
+	generate_walls_along_x(0, DIRECTION_UP)
 	for y in range(size_y):
-		generate_walls_along_x(maze, y, DIRECTION_DOWN, context)
-	generate_walls_along_y(maze, 0, DIRECTION_LEFT, context)
+		generate_walls_along_x(y, DIRECTION_DOWN, 1)
+	walls_y = PoolIntArray()
+	generate_walls_along_y(0, DIRECTION_LEFT)
 	for x in range(size_x):
-		generate_walls_along_y(maze, x, DIRECTION_RIGHT, context)
-	var mesh = MeshInstance.new()
-	mesh.name = "walls"
-	mesh.mesh = ArrayMesh.new()
-	mesh.mesh.add_surface_from_arrays(ArrayMesh.PRIMITIVE_TRIANGLES, [ context.vertices, context.normals, context.tangents, null, context.uvs, null, null, null, null ])
-	mesh.mesh.surface_set_material(0, material)
-	maze.add_child(mesh)
-
-func create_vertical_rect(from, to, context):
-	var uv_from = from.y
-	var uv_to = to.y
-	if abs(from.x-to.x) > abs(from.y-to.y):
-		uv_from = from.x
-		uv_to = to.x
-	var normal = Vector3(from.y-to.y, 0, to.x-from.x).normalized()
-	var tangent = [normal.z, 0, -normal.x, 1]
-	context.add_point(Vector3(from.x, 0, from.y), Vector2(uv_from, 0), normal, tangent)
-	context.add_point(Vector3(from.x, height, from.y), Vector2(uv_from, height), normal, tangent)
-	context.add_point(Vector3(to.x, 0, to.y), Vector2(uv_to, 0), normal, tangent)
-	context.add_point(Vector3(from.x, height, from.y), Vector2(uv_from, height), normal, tangent)
-	context.add_point(Vector3(to.x, height, to.y), Vector2(uv_to, height), normal, tangent)
-	context.add_point(Vector3(to.x, 0, to.y), Vector2(uv_to, 0), normal, tangent)
-
-func create_wall(maze, from, to, context):
-	create_vertical_rect(Vector2(from.x, from.y), Vector2(from.x, to.y), context)
-	create_vertical_rect(Vector2(from.x, to.y), Vector2(to.x, to.y), context)
-	create_vertical_rect(Vector2(to.x, to.y), Vector2(to.x, from.y), context)
-	create_vertical_rect(Vector2(to.x, from.y), Vector2(from.x, from.y), context)
-	var center = 0.5*(from+to)
-	var extent = to-center
-	context.create_collision_shape(center, extent, height)
-
-func generate_walls_along_x(maze, y, d, context):
+		generate_walls_along_y(x, DIRECTION_RIGHT, 1)
+		
+func generate_walls_along_x(y, d, offset = 0):
 	var mask = (1 << d)
-	var offset = 0.5*Vector2(DIRECTIONS[d].x, DIRECTIONS[d].y)
 	var wall_begin = null
 	for x in range(size_x):
 		if wall_begin == null && (cells[x][y] & mask) == 0:
 			wall_begin = x
 		elif wall_begin != null && (cells[x][y] & mask) != 0:
-			create_wall(maze, corridor_width*(Vector2(wall_begin-0.5, y)+offset)-0.5*wall_width*Vector2(1, 1), corridor_width*(Vector2(x-0.5, y)+offset)+0.5*wall_width*Vector2(1, 1), context)
+			walls_x.append(y+offset)
+			walls_x.append(wall_begin)
+			walls_x.append(x-1)
 			wall_begin = null
 	if wall_begin != null:
-		create_wall(maze, corridor_width*(Vector2(wall_begin-0.5, y)+offset)-0.5*wall_width*Vector2(1, 1), corridor_width*(Vector2(size_x-0.5, y)+offset)+0.5*wall_width*Vector2(1, 1), context)
+		walls_x.append(y+offset)
+		walls_x.append(wall_begin)
+		walls_x.append(size_x-1)
 
-func generate_walls_along_y(maze, x, d, context):
+func generate_walls_along_y(x, d, offset = 0):
 	var mask = (1 << d)
-	var offset = 0.5*Vector2(DIRECTIONS[d].x, DIRECTIONS[d].y)
 	var wall_begin = null
 	for y in range(size_y):
 		if wall_begin == null && (cells[x][y] & mask) == 0:
 			wall_begin = y
 		elif wall_begin != null && (cells[x][y] & mask) != 0:
-			create_wall(maze, corridor_width*(Vector2(x, wall_begin-0.5)+offset)-0.5*wall_width*Vector2(1, 1), corridor_width*(Vector2(x, y-0.5)+offset)+0.5*wall_width*Vector2(1, 1), context)
+			walls_y.append(x+offset)
+			walls_y.append(wall_begin)
+			walls_y.append(y-1)
 			wall_begin = null
 	if wall_begin != null:
-		create_wall(maze, corridor_width*(Vector2(x, wall_begin-0.5)+offset)-0.5*wall_width*Vector2(1, 1), corridor_width*(Vector2(x, size_y-0.5)+offset)+0.5*wall_width*Vector2(1, 1), context)
-
-func generate_ceiling(maze, material):
-	if material != null:
-		var context = MeshBuilder.new(maze)
-		var p1 = -(corridor_width+wall_width)*0.5*Vector2(1, 1)
-		var p2 = corridor_width*Vector2(size_x-1, size_y-1)-p1
-		var normal = Vector3(0, -1, 0)
-		var tangent = [ 1, 0, 0, 1 ]
-		context.add_point(Vector3(p1.x, height, p1.y), Vector2(p1.x, p1.y), normal, tangent)
-		context.add_point(Vector3(p1.x, height, p2.y), Vector2(p1.x, p2.y), normal, tangent)
-		context.add_point(Vector3(p2.x, height, p1.y), Vector2(p2.x, p1.y), normal, tangent)
-		context.add_point(Vector3(p2.x, height, p2.y), Vector2(p2.x, p2.y), normal, tangent)
-		var mesh = MeshInstance.new()
-		mesh.name = "floor"
-		mesh.mesh = ArrayMesh.new()
-		mesh.mesh.add_surface_from_arrays(ArrayMesh.PRIMITIVE_TRIANGLE_STRIP, [ context.vertices, context.normals, context.tangents, null, context.uvs, null, null, null, null ])
-		mesh.mesh.surface_set_material(0, material)
-		maze.add_child(mesh)
+		walls_y.append(x+offset)
+		walls_y.append(wall_begin)
+		walls_y.append(size_y-1)
